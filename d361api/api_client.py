a4854377d5aebe1fdfc54d7178a710370ab02d77
Adam Twardoch <adam@twardoch.com>
2025-06-23 13:43:52 +0200

    Update d361api/api_client.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- d361api/api_client.py
+++ d361api/api_client.py
@@ -292,7 +292,7 @@ class ApiClient:
 
         response_type = None
         if response_types_map:
-            response_type = response_types_map.get(str(response_data.status), None)
+            response_type = response_types_map.get(str(response_data.status))
             if not response_type and isinstance(response_data.status, int) and 100 <= response_data.status <= 599:
                 # if not found, look for '1XX', '2XX', etc.
                 response_type = response_types_map.get(str(response_data.status)[0] + "XX", None)
